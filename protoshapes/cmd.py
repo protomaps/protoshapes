078561e6b01ec0702993397bc413091cc097dae0
Brandon Liu <bdon@bdon.org>
2021-06-04 20:44:15 +0800

    columns for admin levels

--- protoshapes/cmd.py
+++ protoshapes/cmd.py
@@ -35,8 +35,8 @@ boundary_values = [
 
 def osm_id(fid):
     if fid % 2 == 0:
-        return f"w {int(fid/2)}"
-    return f"r {int((fid-1)/2)}"
+        return f"way/{int(fid/2)}"
+    return f"relation/{int((fid-1)/2)}"
 
 def rad(degree):
     return degree * math.pi / 180
@@ -67,11 +67,13 @@ class Handler(osmium.SimpleHandler):
             if 'name' in a.tags and a.tags.get('boundary') in boundary_values:
                 wkb = wkbfab.create_multipolygon(a)
                 multipolygon = wkblib.loads(wkb, hex=True)
+                admin_level = a.tags.get("admin_level")
                 area = geodesic_exterior_area(multipolygon)
                 if area > 50000:
-                    self.cursor.execute("INSERT INTO features_original VALUES (?,?,?,?,?,?)",(a.id,a.tags.get("name"),a.tags.get("boundary"),a.tags.get("admin_level"),area,multipolygon.wkb))
+                    self.cursor.execute("INSERT INTO features_original(id,name,boundary,admin_level,area,wkb_geometry) VALUES (?,?,?,?,?,?)",(a.id,a.tags.get("name"),a.tags.get("boundary"),a.tags.get("admin_level"),area,multipolygon.wkb))
                     self.conn.commit()
-                    self.idx.insert(a.id,multipolygon.bounds,obj=area)
+                    if admin_level and int(admin_level) in [2,3,4,5,6,7,8,9,10]:
+                        self.idx.insert(a.id,multipolygon.bounds,obj=int(admin_level))
         except RuntimeError as e:
             print("Error:",a.orig_id())
 
@@ -101,7 +103,7 @@ def main():
     cursor.execute("INSERT INTO spatial_ref_sys VALUES (?,?,?,?)",(0,"epsg",4326,EPSG4326))
     cursor.execute('CREATE TABLE IF NOT EXISTS geometry_columns (f_table_name text, f_geometry_column text, geometry_type integer, coord_dimension integer, srid integer, geometry_format text);')
     cursor.execute("INSERT INTO geometry_columns VALUES (?,?,?,?,?,?)",("features_original","wkb_geometry",6,2,0,"WKB"))
-    cursor.execute('CREATE TABLE IF NOT EXISTS features_original (id integer PRIMARY KEY, name text, boundary text, admin_level integer, area integer, wkb_geometry blob);')
+    cursor.execute('CREATE TABLE IF NOT EXISTS features_original (id integer PRIMARY KEY, name text, boundary text, admin_level integer, area integer, wkb_geometry blob, a2 integer, a3 integer, a4 integer, a5 integer, a6 integer, a7 integer, a8 integer, a9 integer, a10 integer);')
 
     # populate the initial table of geometries
     idx = index.Index()
@@ -115,15 +117,26 @@ def main():
         fid = row[0] 
         area = row[1]
         multipolygon = wkblib.loads(row[2])
-        intersections = [(i.id,i.object) for i in idx.intersection(multipolygon.bounds,objects=True) if (i.id != fid and i.object > area)]
+        intersections = [(i.id,i.object) for i in idx.intersection(multipolygon.bounds,objects=True) if (i.id != fid)]
         candidates = [v[0] for v in sorted(intersections,key=lambda x:x[1])]
+        within = [None,None,None,None,None,None,None,None,None]
         while len(candidates) > 0:
             candidate = candidates.pop(0)
-            subcursor.execute("SELECT wkb_geometry FROM features_original WHERE id = ?",(candidate,))
+            subcursor.execute("SELECT admin_level,wkb_geometry,a2,a3,a4,a5,a6,a7,a8,a9,a10 FROM features_original WHERE id = ?",(candidate,))
             candidate_row = subcursor.fetchone()
-            candidate_poly = wkblib.loads(candidate_row[0])
+            candidate_poly = wkblib.loads(candidate_row[1])
+            candidate_admins = candidate_row[2:11]
+            admin_level = candidate_row[0]
             if multipolygon.within(candidate_poly):
-                pass
+                # if within[admin_level-2]:
+                #     print(osm_id(fid),"in",osm_id(within[admin_level-2]),"or",osm_id(candidate))
+                within[admin_level-2] = candidate
+                for i in range(admin_level+1,11):
+                    if candidate_admins[i-2]:
+                        within[i-2] = candidate_admins[i-2]
+                        candidates.remove(candidate_admins[i-2])
+        subcursor.execute("UPDATE features_original SET a2=?, a3=?,a4=?,a5=?,a6=?,a7=?,a8=?,a9=?,a10=? WHERE id = ?",within + [fid])
+        conn.commit()
 
     print("Elapsed: ", time.time() - start)
     conn.close()
